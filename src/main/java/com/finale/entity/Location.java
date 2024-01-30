d619170af9a50fdd2a62cdffc4b603289868fc02
Drum-J <tmdgh717@naver.com>
2024-01-30 17:44:51 +0900

    refactor. 불필요한 import 제거

--- src/main/java/com/finale/entity/Location.java
+++ src/main/java/com/finale/entity/Location.java
@@ -1,6 +1,5 @@
 package com.finale.entity;
 
-import com.finale.entity.TimeStamped;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
