d21108ae26fe9bc894b6b6ac7794a34efe406086
승호 <tmdgh717@naver.com>
2024-05-29 22:33:16 +0900

    feat. 카카오 로그인 처리 방식 변경

--- src/main/java/com/finale/login/controller/LoginController.java
+++ src/main/java/com/finale/login/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.finale.login.controller;
 
+import com.finale.common.ApiResponse;
 import com.finale.login.dto.KakaoUserInfo;
 import com.finale.login.service.KakaoAPIService;
 import com.finale.login.service.LoginService;
@@ -7,17 +8,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/login")
 public class LoginController {
@@ -29,14 +30,13 @@ public class LoginController {
     private static final String COACH = "coach";
 
     @GetMapping("/{type}")
-    public void studentLogin(HttpServletResponse response, @PathVariable("type") String type) throws IOException {
-        String uri = kakaoAPIService.redirectUri(type);
-        response.sendRedirect(uri);
+    public ApiResponse studentLogin(@PathVariable("type") String type) {
+        return ApiResponse.successResponse(kakaoAPIService.redirectUri(type));
     }
 
     @ResponseBody
     @GetMapping("/callback")
-    public String studentCallback(@RequestParam(name = "code") String code,
+    public ApiResponse studentCallback(@RequestParam(name = "code") String code,
                                   @RequestParam(name = "state") String type,
                                   HttpServletResponse response) {
         try {
@@ -47,13 +47,13 @@ public class LoginController {
             } else if (type.equals(COACH)) {
                 token = loginService.loginCoach(userInfo);
             } else {
-                return "ERROR";
+                return ApiResponse.errorResponse("Type 값이 올바르지 않습니다.");
             }
 
             response.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + token);
-            return "SUCCESS : " + token;
+            return ApiResponse.successResponse(token);
         } catch (IOException e) {
-            return e.getMessage();
+            return ApiResponse.badRequestResponse(e.getMessage());
         }
     }
 }
