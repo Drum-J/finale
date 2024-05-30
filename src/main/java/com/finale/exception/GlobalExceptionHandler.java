5f9957f292073cd8411a400670a9d89bced0a546
seungho <tmdgh717@naver.com>
2024-05-30 16:45:11 +0900

    refactor. 에러 응답시 ResponseEntity로 변경

--- src/main/java/com/finale/exception/GlobalExceptionHandler.java
+++ src/main/java/com/finale/exception/GlobalExceptionHandler.java
@@ -1,24 +1,27 @@
 package com.finale.exception;
 
 import com.finale.common.ApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ApiResponse resourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
-        return ApiResponse.notFoundResponse(exception.getMessage());
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException exception) {
+        return ResponseEntity.status(NOT_FOUND).body(ApiResponse.notFoundResponse(exception.getMessage()));
     }
 
     @ExceptionHandler(IllegalStateException.class)
-    public ApiResponse duplicateExceptionHandler(IllegalStateException exception) {
-        return ApiResponse.badRequestResponse(exception.getMessage());
+    public ResponseEntity<ApiResponse> duplicateExceptionHandler(IllegalStateException exception) {
+        return ResponseEntity.badRequest().body(ApiResponse.badRequestResponse(exception.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
-    public ApiResponse allExceptionHandler(Exception exception) {
-        return ApiResponse.errorResponse(exception.getMessage());
+    public ResponseEntity<ApiResponse> allExceptionHandler(Exception exception) {
+        return ResponseEntity.internalServerError().body(ApiResponse.errorResponse(exception.getMessage()));
     }
 }
