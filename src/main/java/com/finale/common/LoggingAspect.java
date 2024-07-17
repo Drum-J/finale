c647cc539656e9151eb9198300cf98a349c4a0ea
DrumJ <tmdgh717@naver.com>
2024-07-17 23:13:12 +0900

    feat. Spring AOP 적용

---
+++ src/main/java/com/finale/common/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.finale.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("execution(* com.finale..*(..))") // com.finale 패키지 아래의 모든 메서드에 적용
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+
+        log.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
+
+        return proceed;
+    }
+}
