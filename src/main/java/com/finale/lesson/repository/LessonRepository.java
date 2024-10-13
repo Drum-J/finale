403aeb0f75dc986e8ce2793977af8fda8b745f67
DrumJ <tmdgh717@naver.com>
2024-10-13 18:32:35 +0900

    refactor. location 삭제 시 관련 lesson 삭제 쿼리 변경

--- src/main/java/com/finale/lesson/repository/LessonRepository.java
+++ src/main/java/com/finale/lesson/repository/LessonRepository.java
@@ -4,6 +4,7 @@ import com.finale.entity.Lesson;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,9 +17,10 @@ public interface LessonRepository extends JpaRepository<Lesson, Long> {
 
     List<Lesson> findByLessonDateAndDelYnIsFalse(String lessonDate);
 
+    @Modifying
     @Query(value =
-            "UPDATE Lesson l JOIN Timetable t ON l.timetable_id = t.timetable_id" +
-            " SET l.delYn = TRUE WHERE t.location = :locationName"
+            "UPDATE lesson l JOIN timetable t ON l.timetable_id = t.timetable_id" +
+            " SET l.del_yn = TRUE WHERE t.location = :locationName"
             ,nativeQuery = true)
-    void deleteLessonByLocationName(String locationName);
+    void deleteLessonByLocationName(@Param("locationName") String locationName);
 }
